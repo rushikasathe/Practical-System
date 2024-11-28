da1bc6bfc97f032284c924a1b0e2a01a0b6a76cc
rushikasathe <rushikasathe@gmail.com>
2024-11-28 12:00:43 +0530

    index.js

--- api/index.js
+++ api/index.js
@@ -17,9 +17,12 @@ app.use(express.json());
 app.use("/api/v1", router);
 
 dbConnect();
-app.get("/",(req,res)=>{
-  res.send("Hello");
-});
+app.get("/", (req, res)=>{
+  res.json({
+    success: true,
+    message: "Server Running Successfully"
+  })
+})
 
 app.listen(PORT, () => {
   console.log(`Server is running at port: ${PORT}`);
